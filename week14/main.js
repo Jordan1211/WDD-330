fa6b7e2db5718d09b57f244e3704da83309e2840
Jordan1211 <90577914+Jordan1211@users.noreply.github.com>
2022-07-14 07:56:56 -0600

    update final

--- week14/main.js
+++ week14/main.js
@@ -4,3 +4,8 @@ document.getElementById('submit-schedule').addEventListener('click', () => {
   async function format() {
     // Code to format calendar
   }
+
+  const p1week1 = form.querySelectorAll('.parent1 .week1 input');
+  const p1week2 = form.querySelectorAll('.parent1 .week2 input');
+  const p2week1 = form.querySelectorAll('.parent2 .week1 input');
+  const p2week2 = form.querySelectorAll('.parent2 .week2 input');
